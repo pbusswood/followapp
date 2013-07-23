9a23f87d0826d071493900571b21a4ea1cc648e7
Parker Busswood <parkerbusswood@gmail.com>
2013-07-23 11:27:03 -0400

    Added user model tests, associated validations, and user fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,18 @@ class User < ActiveRecord::Base
   DeviseController.class_eval do
     def resource_params
       unless params[resource_name].blank?
-        params.require(resource_name).permit(:email, :password, :password_confirmation, :remember_me)
+        params.require(resource_name).permit(:email, :password, :password_confirmation,
+                                             :remember_me, :first_name, :last_name, :username)
       end
     end
   end
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true,
+                       uniqueness: true,
+                       format: {
+                         with: /\A[a-zA-Z0-9_-]+\z/,
+                         message: 'Must be formatted correctly.'
+                       }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a user should enter a first name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter a last name" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter a username" do
+    user = User.new
+    assert !user.save
+    assert !user.errors[:username].empty?
+  end
+
+  test "a user should enter a unique username" do
+    user = User.new
+    user.username = users(:parker).username
+
+    assert !user.save
+    assert !user.errors[:username].empty?
+  end
+
+  test "a user should have a profile name without spaces" do
+    user = User.new(first_name: 'Parker', last_name: 'Busswood', email: 'parkerbusswood2@gmail.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+
+    user.username = "My Username"
+
+    assert !user.save
+    assert !user.errors[:username].empty?
+    assert user.errors[:username].include?("Must be formatted correctly.")
+  end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Parker', last_name: 'Busswood', email: 'parkerbusswood2@gmail.com')
+    user.password = user.password_confirmation = 'asdfasdf'
+
+    user.username = 'parkerbusswood_1'
+    assert user.valid?
+  end
 end
