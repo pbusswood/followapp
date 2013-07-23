414e6575f482ab531e34ce785e39fe24d82ddef0
Parker Busswood <parkerbusswood@gmail.com>
2013-07-23 16:28:00 -0400

    Added assocations for user and contacts, and updated contact forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  DeviseController.class_eval do
-    def resource_params
-      unless params[resource_name].blank?
-        params.require(resource_name).permit(:email, :password, :password_confirmation,
-                                             :remember_me, :first_name, :last_name, :username)
-      end
-    end
-  end
-
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, presence: true,
@@ -22,6 +13,8 @@ class User < ActiveRecord::Base
                          with: /\A[a-zA-Z0-9_-]+\z/,
                          message: 'Must be formatted correctly.'
                        }
+
+  has_many :contacts
 
   def full_name
     first_name + " " + last_name
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -244,3 +244,12 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
 end
+
+DeviseController.class_eval do
+  def resource_params
+    unless params[resource_name].blank?
+      params.require(resource_name).permit(:email, :password, :password_confirmation,
+                                           :remember_me, :first_name, :last_name, :username)
+    end
+  end
+end
