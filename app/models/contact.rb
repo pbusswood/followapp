88fcc146b24de5cc9431d722a087f10d583fd3b7
lucasjmoore <lucasmoore88@gmail.com>
2013-07-30 10:27:23 -0400

    added validates for contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,24 @@
 class Contact < ActiveRecord::Base
   belongs_to :user
+ 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true,
+                       format: {
+                         with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i,
+                         message: "The email must be formatted correctly."
+                       }
+   validates :phone,
+   				format: {
+   					with: /\A[0-9()-]+\z/,
+   					message: "Phone numbers can't have letters"
+   				}
+   validates :mobile,
+   				format: {
+   					with: /\A[0-9()-]+\z/,
+   					message: "Mobile phone numbers can't have letters"
+   				}
 end
+
+
+#make phone validations work
